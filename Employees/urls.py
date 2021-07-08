43380fcbe1f618418aebbca3c7273627c61eaa6c
remeolong <remeolong82@yahoo.com>
2021-07-08 18:53:58 -0500

    updated index html, added machines brand, models, types and added links to service manual for weatherford machines.

--- Employees/urls.py
+++ Employees/urls.py
@@ -4,6 +4,27 @@ from .views import *
 
 urlpatterns = [
    path('', views.home, name='Home'),
+   path('weatherford/test_stand', views.weatherford_test_stand, name="TestStand"),
+   path('weatherford/performance_test', views.weatherford_performance_test, name="PerformanceTest"),
+   path('tool_changer', views.tool_changer, name="ToolChanger"),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+#path('', views., name=""),
+
+
 
 
 ]
--- Employees/views.py
+++ Employees/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 @login_required
 def home(request):
     return render(request, 'index/index.html', {})
+
+
+def weatherford_performance_test(request):
+    return render(request, 'index/weatherford_performance_test.html', {})
+
+
+def weatherford_test_stand(request):
+    return render(request, 'index/weatherford_test_stand.html', {})
+
+
+def tool_changer(request):
+    return render(request, 'index/tool_changer.html', {})
